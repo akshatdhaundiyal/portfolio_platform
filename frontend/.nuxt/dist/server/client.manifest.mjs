c9cd16d088603630e3222ae1e0da45ff8385b000
Akshat Dhaundiyal <akshatdhaundiyal10@gmail.com>
2026-04-12 00:04:48 +0530

    Delete frontend/.nuxt directory

--- frontend/.nuxt/dist/server/client.manifest.mjs
+++
@@ -1,4 +0,0 @@
-
-import { viteNodeFetch } from "file:///D:/self_work/projects/portfolio_platform/frontend/node_modules/@nuxt/vite-builder/dist/vite-node.mjs"
-export default () => viteNodeFetch.getManifest()
-    
--- frontend/.nuxt/dist/server/runner.mjs
+++
@@ -1,1 +0,0 @@
-export { default } from "file:///D:/self_work/projects/portfolio_platform/frontend/node_modules/@nuxt/vite-builder/dist/vite-node-runner.mjs"
