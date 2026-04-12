0bd0b4e5fb656ccd79b5493af84ffe0fa9ef38cb
akshatdhaundiyal <akshatdhaundiyal10@gmail.com>
2026-04-12 16:57:33 +0530

    upgrade to nuxt4

--- frontend/app/app.vue
+++ frontend/app/app.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="min-h-screen bg-gray-50 dark:bg-gray-950 font-sans text-gray-900 dark:text-gray-100">
+  <div class="min-h-screen flex flex-col bg-gray-50 dark:bg-gray-950 font-sans text-gray-900 dark:text-gray-100">
     <NuxtLayout>
       <NuxtRouteAnnouncer />
       <NuxtPage />
